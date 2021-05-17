2edacda240d7c4dfca207a0052f228d547415b80
Adelina <adelina.romascu@gmail.com>
2021-05-17 17:29:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class LS
+{
+	public static int search(int[] arr, int x)
+	{
+		int n = arr.Length;
+		for (int i = 0; i < n; i++)
+		{
+			if (arr[i] == x)
+				return i;
+		}
+		return -1;
+	}
+
+	
+	public static void Main()
+	{
+		int[] arr = { 2, 3, 4, 10, 40 };
+		int x = 10;
+
+		// Apelarea functiei
+		int result = search(arr, x);
+		if (result == -1)
+			Console.WriteLine(
+				"Elementul nu este prezent in tablou");
+		else
+			Console.WriteLine("Elementul este prezent in index "
+							+ result);
+	}
+}
+
+
